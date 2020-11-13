deb43e86560e3b0595c695f31f5691a976859a51
Francois-Rene Rideau <fare@tunes.org>
2020-11-12 19:37:27 -0500

    gambit-unstable: 2020-09-20 -> 2021-02-25

--- pkgs/development/compilers/gambit/unstable.nix
+++ pkgs/development/compilers/gambit/unstable.nix
@@ -1,13 +1,13 @@
 { callPackage, fetchFromGitHub, gambit-support }:
 
 callPackage ./build.nix {
-  version = "unstable-2020-09-20";
-  git-version = "4.9.3-1234-g6acd87df";
+  version = "unstable-2021-02-25";
+  git-version = "4.9.3-1305-gce9f6964";
   src = fetchFromGitHub {
     owner = "feeley";
     repo = "gambit";
-    rev = "6acd87dfa95bfca33082a431e72f023345dc07ee";
-    sha256 = "0a3dy4ij8hzlp3sjam4b6dp6yvyz5d7g2x784qm3gp89fi2ck56r";
+    rev = "ce9f696445ad948b2e7a05701ef42879d8dc5a8a";
+    sha256 = "0jl297csc5sc2s7d1505mh06pkshhx147xndzm8ld0y6dws0zjmb";
   };
   gambit-params = gambit-support.unstable-params;
 }
