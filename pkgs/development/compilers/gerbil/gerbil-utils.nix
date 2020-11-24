d32e762946078a16ea4cf75734f0ba8f8ac540b9
Francois-Rene Rideau <fare@tunes.org>
2020-11-24 09:15:16 -0500

    gerbilPackages-unstable.gerbil-utils: 2020-10-18 -> 2021-12-20

--- pkgs/development/compilers/gerbil/gerbil-utils.nix
+++ pkgs/development/compilers/gerbil/gerbil-utils.nix
@@ -1,25 +1,26 @@
-{ lib, fetchFromGitHub, gerbil-unstable, gerbil-support, gambit-support }:
+{ lib, fetchFromGitHub, ... }:
+
 {
   pname = "gerbil-utils";
-  version = "unstable-2020-10-18";
-  git-version = "0.2-36-g8b481b7";
-  gerbil-package = "clan";
-  gerbil = gerbil-unstable;
-  gambit-params = gambit-support.unstable-params;
-  version-path = "version";
+  version = "unstable-2021-12-20";
+  git-version = "0.2-150-ga989129";
   softwareName = "Gerbil-utils";
+  gerbil-package = "clan";
+  version-path = "version";
+
   pre-src = {
     fun = fetchFromGitHub;
     owner = "fare";
     repo = "gerbil-utils";
-    rev = "8b481b787e13e07e14d0718d670aab016131a090";
-    sha256 = "0br8k5b2wcv4wcp65r2bfhji3af2qgqjspf41syqslq9awx47f3m";
+    rev = "a989129086122f0e954809ede588682e709a5500";
+    sha256 = "1l8i3afqzq3bs71c133g72ywszg4azyadp7ac8r1v3aszb1xqk1a";
   };
-  meta = {
+
+  meta = with lib; {
     description = "Gerbil Clan: Community curated Collection of Common Utilities";
     homepage    = "https://github.com/fare/gerbil-utils";
-    license     = lib.licenses.lgpl21;
-    platforms   = lib.platforms.unix;
-    maintainers = with lib.maintainers; [ fare ];
+    license     = licenses.lgpl21;
+    platforms   = platforms.unix;
+    maintainers = with maintainers; [ fare ];
   };
 }
