83fce3985c34d575c4a957b9b4e3362035eb55bc
Francois-Rene Rideau <fare@tunes.org>
2020-11-24 09:18:56 -0500

    gerbilPackages-unstable.gerbil-ethereum: 2020-10-18 -> 2021-02-26

--- pkgs/development/compilers/gerbil/gerbil-ethereum.nix
+++ pkgs/development/compilers/gerbil/gerbil-ethereum.nix
@@ -1,23 +1,22 @@
-{ pkgs, lib, fetchFromGitHub, gerbil-unstable, gerbil-support, gambit-support }:
+{ lib, fetchFromGitHub, gerbilPackages, ... }:
 {
   pname = "gerbil-ethereum";
-  version = "unstable-2020-10-18";
-  git-version = "0.0-26-gf27ada8";
-  gerbil-package = "mukn/ethereum";
-  gerbil = gerbil-unstable;
-  gerbilInputs = with gerbil-support.gerbilPackages-unstable;
-    [gerbil-utils gerbil-crypto gerbil-poo gerbil-persist];
-  buildInputs = [];
-  gambit-params = gambit-support.unstable-params;
-  version-path = "version";
+  version = "unstable-2021-02-26";
+  git-version = "0.0-203-gc970a44";
   softwareName = "Gerbil-ethereum";
+  gerbil-package = "mukn/ethereum";
+  version-path = "version";
+  gerbilInputs = with gerbilPackages; [gerbil-utils gerbil-crypto gerbil-poo gerbil-persist];
   pre-src = {
     fun = fetchFromGitHub;
     owner = "fare";
     repo = "gerbil-ethereum";
-    rev = "f27ada8e7f4de4f8fbdfede9fe055914b254d8e7";
-    sha256 = "1lykjqim6a44whj1r8kkpiz68wghkfqx5vjlrc2ldxlmgd4r9gvd";
+    rev = "c970a443d3fcbbb3f341fab780ea5ef66c7f01ad";
+    sha256 = "168n2c1wpmdigp3izl3vhnm5am5hjf7bn6xy8zzwpkmk13g566z4";
   };
+  postInstall = ''
+    cp scripts/{croesus.prv,genesis.json,logback.xml,yolo-evm.conf,yolo-kevm.conf} $out/gerbil/lib/mukn/ethereum/scripts/
+    '';
   meta = {
     description = "Gerbil Ethereum: a Scheme alternative to web3.js";
     homepage    = "https://github.com/fare/gerbil-ethereum";
