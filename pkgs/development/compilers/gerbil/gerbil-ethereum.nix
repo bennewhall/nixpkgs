555e99ceebb0f7e0b468a215c8d91aa3acd6be77
Noel Kwan <noelkwan1998@gmail.com>
2021-06-19 02:07:23 +0800

    gambit:                                  2021-06-04 -> 2021-06-18 gerbilPackages-unstable.gerbil-ethereum: 2021-06-03 -> 2021-06-18 gerbilPackages-unstable.glow:            2021-06-04 -> 2021-06-18 gerbilPackages-unstable.smug-gerbil:     2020-12-12 -> 2020-12-12 (git-version: cf23a47 -> 0.4.20) gerbilPackages-unstable:                 2021-05-28 -> 2021-06-08

--- pkgs/development/compilers/gerbil/gerbil-ethereum.nix
+++ pkgs/development/compilers/gerbil/gerbil-ethereum.nix
@@ -2,8 +2,8 @@
 
 rec {
   pname = "gerbil-ethereum";
-  version = "unstable-2021-06-03";
-  git-version = "0.0-283-g8e6c103";
+  version = "unstable-2021-06-18";
+  git-version = "0.0-285-gbb6dc2c";
   softwareName = "Gerbil-ethereum";
   gerbil-package = "mukn/ethereum";
   version-path = "version";
@@ -14,8 +14,8 @@ rec {
     fun = fetchFromGitHub;
     owner = "fare";
     repo = "gerbil-ethereum";
-    rev = "8e6c1036bdde0d495484bd0d96526405cc1d51a3";
-    sha256 = "0h6fbn8q8iqyq0ynky2nbl7qz126nv9lmayjv234g0vbc8vh9ylz";
+    rev = "bb6dc2c6390cb12078edbaf762519ff938ac4579";
+    sha256 = "0x9fbgm7pwrqdnn68sa2xynb1gfahzi1c6hqbmhgnbyh9dw807i0";
   };
 
   postInstall = ''
