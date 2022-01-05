40f7c9e8618db15a673607f0811a7b2f3180eaab
AlexKnauth <alexander@knauth.org>
2022-01-05 17:45:25 -0500

    gerbil-unstable: 2021-12-20 -> 2022-01-05

--- pkgs/development/compilers/gerbil/unstable.nix
+++ pkgs/development/compilers/gerbil/unstable.nix
@@ -1,13 +1,13 @@
 { callPackage, fetchFromGitHub, gambit-unstable, gambit-support }:
 
 callPackage ./build.nix rec {
-  version = "unstable-2021-12-20";
+  version = "unstable-2022-01-05";
   git-version = "0.16-267-g32e5bbf6";
   src = fetchFromGitHub {
     owner = "vyzo";
     repo = "gerbil";
-    rev = "32e5bbf694b5954075f056969214afc39ca844f4";
-    sha256 = "165l83ss7v9gjg5agbg9xsimzkyqb4wcvsi1qsjzjvavm9b329yh";
+    rev = "3dd0d0384b0148bb6c82fc8e4efb4ca3f893acf2";
+    sha256 = "sha256-q6wDEIGdtXt1shDPscKPgYZLnUr7K83aplTrL0A9p34=";
   };
   inherit gambit-support;
   gambit = gambit-unstable;
